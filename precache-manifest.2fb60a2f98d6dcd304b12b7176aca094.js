5d577fc3da378d2c842011ce0a6bec8b888c8dd0
Alex Stout <astoutj@gmail.com>
2019-10-02 15:38:35 -0600

    Updates

---
+++ precache-manifest.2fb60a2f98d6dcd304b12b7176aca094.js
@@ -0,0 +1,39 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d56a3a5d2030f42b80ed8f920501ac8b",
+    "url": "31cc4430e108c3edfaf1.worker.js"
+  },
+  {
+    "revision": "9e35cfd25cdfb0bd40f9",
+    "url": "css/0.css"
+  },
+  {
+    "revision": "3ba7f21ad749396409083a67e1d650a8",
+    "url": "index.html"
+  },
+  {
+    "url": "js/main.e7dab800bcbd4099fbc6.js"
+  },
+  {
+    "url": "js/runtime.e7dab800bcbd4099fbc6.js"
+  },
+  {
+    "url": "js/vendor.e7dab800bcbd4099fbc6.js"
+  },
+  {
+    "revision": "3de28cd3b11aa991a56dfbfb8b2ce3ef",
+    "url": "public/icon.ico"
+  },
+  {
+    "revision": "8c6194c258af8ab1c446ce4c1d7bcdb3",
+    "url": "public/images/image_1.jpg"
+  },
+  {
+    "revision": "a6db2f1d372f474a11480f89169abafa",
+    "url": "public/images/image_2.jpg"
+  },
+  {
+    "revision": "178d116173c5779013416aab061cae5a",
+    "url": "public/images/image_3.jpg"
+  }
+]);
